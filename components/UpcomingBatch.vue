17e2e9f132f10c636f8fe9e2144ad4d18733550a
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2024-12-11 19:01:28 +0530

    upcoming batch added

---
+++ components/UpcomingBatch.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="pb-3">
+    <section>
+      <h1 class="text-3xl font-bold">Upcoming Batch</h1>
+    </section>
+    <div class="shadow-md rounded-lg overflow-auto">
+      <table class="w-full text-left">
+        <thead
+          class="uppercase bg-[#FF8343] text-white"
+          style="background-color: #ff8343; color: #fff"
+        >
+          <tr>
+            <!--[-->
+            <td class="py-1 border text-center font-bold p-4">Date</td>
+            <td class="py-1 border text-center font-bold p-4">Time</td>
+            <td class="py-1 border text-center font-bold p-4">Branch</td>
+            <td class="py-1 border text-center font-bold p-4">Trainer</td>
+            <td class="py-1 border text-center font-bold p-4">Batch Code</td>
+            <!--]-->
+          </tr>
+        </thead>
+        <tbody class="text-black bg-[#FFFFFF]">
+          <!--[-->
+          <tr class="py-1">
+            <!--[-->
+            <td class="py-1 border text-center p-4 whitespace-nowrap">
+              YY-853581
+            </td>
+            <td class="py-1 border text-center p-4 whitespace-nowrap">
+              Notebook Basic
+            </td>
+            <td class="py-1 border text-center p-4 whitespace-nowrap">$ 299</td>
+            <td class="py-1 border text-center p-4 whitespace-nowrap">
+              YY-853599
+            </td>
+            <td class="py-1 border text-center p-4 whitespace-nowrap">
+              Notebook Pro
+            </td>
+            <!--]-->
+          </tr>
+          <!--]-->
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
