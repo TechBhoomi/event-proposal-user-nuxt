6d1ff5c65d64111dbfaf08351ede5e72c89a4409
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2024-12-11 19:01:47 +0530

    default layout added for the nuxt page

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<!-- layouts/default.vue -->
+<template>
+  <div>
+    <header class="sticky top-0">
+      <NavHeader />
+      <Navbar />
+    </header>
+    <!-- <nuxt /> -->
+    <slot />
+    <!-- <footer class="w-full"> -->
+    <!-- </footer> -->
+  </div>
+</template>
+
+<style scoped></style>
