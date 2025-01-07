11bffebf3e9b3c556f3a10467e582d2c7864795c
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2025-01-07 20:01:03 +0530

    get courses and get course by id added

--- store/globalStore.js
+++ store/globalStore.js
@@ -7,6 +7,15 @@ export const useGlobalStore = defineStore("globalStore", () => {
   const URL = import.meta.env.VITE_BASE_URL;
   const eventData = ref([]);
   const isEventApiLoading = ref(false);
+  // course api
+  const courseData = ref(null);
+  const isCouseDataLoading = ref(false);
+  const hasCourseApiError = ref(false);
+  // get course by id
+  const courseByID = ref([]);
+  const getCourseByIdLoading = ref(false);
+  const hasCourseByIdError = ref(false);
+  // get event
   async function getEventData(uuid) {
     try {
       let url = `${URL}/api/v1/get_events`;
@@ -28,5 +37,50 @@ export const useGlobalStore = defineStore("globalStore", () => {
       isEventApiLoading.value = false;
     }
   }
-  return { count, getEventData, eventData, isEventApiLoading, apiError };
+  // get all courses
+  async function getCourse() {
+    try {
+      const response = await axios.get(
+        "https://golang.qspiders.com/backend/api/v1/categories/getAllCategories?organization=jspiders"
+      );
+      courseData.value = response?.data?.data[0]?.courseResponse;
+    } catch (error) {
+      hasCourseApiError.value = error.response;
+    } finally {
+      isCouseDataLoading.value = false;
+    }
+  }
+  // get course by id
+  async function getCourseById(course_Id) {
+    let URL = `https://golang.qspiders.com/backend/api/v1/courses/getbyid`;
+    if (course_Id) {
+      URL += `?courseId=${course_Id}`;
+      console.log(URL);
+      try {
+        const response = await axios.get(URL);
+        courseByID.value = response.data?.data?.details;
+      } catch (error) {
+        hasCourseByIdError.value = error.response;
+      } finally {
+        getCourseById, (getCourseByIdLoading.value = false);
+      }
+    } else {
+      throw new Error("Please provide a valid course ID");
+    }
+  }
+  return {
+    count,
+    getEventData,
+    eventData,
+    isEventApiLoading,
+    apiError,
+    getCourse,
+    isCouseDataLoading,
+    hasCourseApiError,
+    courseData,
+    getCourseById,
+    courseByID,
+    getCourseByIdLoading,
+    hasCourseByIdError,
+  };
 });
