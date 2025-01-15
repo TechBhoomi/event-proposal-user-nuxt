edb5776642ad8cdb10b4fafc6f1c97960f9d082b
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2025-01-15 20:31:09 +0530

    styling changes

--- components/Curiculum.vue
+++ components/Curiculum.vue
@@ -14,7 +14,7 @@
           v-for="sub in courseData"
           @click="getCourseID(sub)"
           :key="sub"
-          class="bg-blue-100 hover:bg-blue-200 text-blue-800 font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 inline-flex items-center justify-center cursor-pointer text-base"
+          class="bg-blue-100 hover:bg-blue-200 text-blue-800 font-semibold me-2 px-4 py-0.5 dark:bg-gray-700 dark:text-blue-400 border rounded-md border-blue-400 inline-flex items-center justify-center cursor-pointer text-base"
         >
           {{ sub?.title }}
         </section>
