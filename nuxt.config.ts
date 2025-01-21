ae90023c65bf325167bd1104a01fd780347e7f18
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2025-01-21 19:34:07 +0530

    index css configuration added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "vue3-carousel-nuxt",'nuxt-pdfeasy'],
-  css: ["~/assets/css/tailwind.css"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "vue3-carousel-nuxt",
+    "nuxt-pdfeasy",
+  ],
+  css: ["~/assets/css/tailwind.css", "~/assets/css/index.css"],
 });
