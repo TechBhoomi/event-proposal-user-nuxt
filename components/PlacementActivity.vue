5d72aedf692e95bb31449af7f9088d51b95da76a
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2025-01-29 16:38:15 +0530

    placement activity url made dynamic

--- components/PlacementActivity.vue
+++ components/PlacementActivity.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg-[#EBEBEB] rounded-md p-2 flex flex-col">
-    <section>
-      <h1 class="lg:text-3xl text-xl font-bold w-full text-center">Placement Activities</h1>
+    <section class="w-full">
+      <h1 class="lg:text-3xl text-xl font-bold w-full text-center">
+        Placement Activities
+      </h1>
     </section>
     <section>
       <div class="grid lg:grid-cols-5 grid-cols-3 px-2.5 py-2.5">
@@ -10,10 +12,7 @@
           v-for="(image, imageIndex) in images"
           :key="imageIndex"
         >
-          <img
-            :src="`https://gotest.qspiders.com/api/v1/get_image?name=${image}`"
-            alt=""
-          />
+          <img :src="`${URL}/api/v1/get_image?name=${image}`" alt="" />
         </div>
       </div>
     </section>
@@ -24,6 +23,7 @@
 const props = defineProps({
   images: { type: Array, required: true },
 });
+const URL = import.meta.env.VITE_BASE_URL;
 </script>
 
 <style scoped></style>
