2eba08f1ac7c1e33da70ae1c27398ac6064869e3
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2025-01-31 18:36:01 +0530

    font size changed for mobile devices

--- components/Greeting.vue
+++ components/Greeting.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="p-4 bg-[#EBEBEB] mb-2">
-    <div class="text-3xl p-2 font-bold font-lexend">
+    <div class="lg:text-3xl text-2xl p-2 font-bold font-lexend">
       <h1>Hello {{ userName }},</h1>
       <h1 v-for="(greeting, index) in data" :key="index">{{ greeting }}</h1>
     </div>
