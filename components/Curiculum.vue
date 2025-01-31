b7e781c709596a2a6caf468534ca8db8d39cbc43
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2025-01-31 18:31:30 +0530

    styles improved

--- components/Curiculum.vue
+++ components/Curiculum.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="mb-2">
     <CourseSubTopics
       :subjectData="courseByID"
       v-if="isSubTopicModalOpen"
@@ -10,32 +10,64 @@
     <article
       class="p-2 flex items-center justify-center flex-col bg-[#EBEBEB] mb-2"
     >
-      <div class="text-2xl font-bold">Our Popular Courses</div>
+      <div class="text-3xl p-2 font-bold font-lexend">Our Popular Courses</div>
       <div
         class="grid lg:grid-cols-4 lg:gap-4 xl:grid-cols-5 xl:gap-4 2xl:grid-cols-5 2xl:gap-8 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 p-4 gap-4 mx-0"
       >
         <div
           v-for="sub in courseData"
           :key="sub"
-          class="bg-white font-semibold text-center rounded-2xl border shadow-lg p-3 lg:max-w-[2-rem] flex items-center justify-between flex-col"
+          class="bg-white font-semibold text-center rounded-2xl border shadow-lg p-3 lg:max-w-[2-rem] flex items-center justify-between flex-col group"
         >
           <img
-            class="mb-3 h-24 rounded-md mx-auto"
+            class="mb-3 h-28 rounded-md mx-auto group-hover:scale-110 duration-200 ease-in-out"
             :src="sub?.icon"
             alt="course"
           />
-          <h1 class="text-lg text-gray-700 font-bold">
+          <h1 class="text-lg text-gray-700 font-bold font-lexend">
             {{ sub?.title }}
           </h1>
           <p class="text-sm text-gray-400 mt-2 text-justify basis-[70%]">
             {{ sub?.description }}
           </p>
-          <button
+          <!-- <button
             @click="getCourseID(sub)"
             type="button"
             class="px-5 py-3 text-xs font-medium text-center text-white rounded-lg bg-[#F96E2A]"
           >
             Learn More
+          </button> -->
+          <button
+            @click="getCourseID(sub)"
+            type="button"
+            class="relative inline-flex items-center justify-center px-3.5 py-2 overflow-hidden font-medium text-[#F96E2A] transition duration-300 ease-out border-2 border-[#F96E2A] rounded-lg shadow-md group"
+          >
+            <span
+              class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#F96E2A] group-hover:translate-x-0 ease"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                ></path>
+              </svg>
+            </span>
+            <span
+              class="absolute flex items-center text-base font-semibold justify-center w-full h-full text-[#F96E2A] transition-all duration-300 transform group-hover:translate-x-full ease"
+            >
+              Learn More</span
+            >
+            <span class="relative text-base font-semibold invisible">
+              Learn More</span
+            >
           </button>
         </div>
       </div>
