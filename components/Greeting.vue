c3ab8456c8fc673bfa3db01383fa7219128ba027
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2025-02-13 16:03:32 +0530

    changes done

--- components/Greeting.vue
+++ components/Greeting.vue
@@ -1,14 +1,8 @@
 <template>
   <section class="p-4 mb-2 flex items-center justify-center flex-col w-full">
     <div class="lg:text-3xl text-2xl p-2 font-bold font-lexend">
-      <h1 class="w-full text-center">{{ userName }},</h1>
-      <h1
-        v-for="(greeting, index) in data"
-        :key="index"
-        class="text-[#f5861f] text-center"
-      >
-        {{ greeting }}
-      </h1>
+      <h1 class="w-full text-center">Hello {{ userName }},</h1>
+      <h1 class="text-[#f5861f] text-center">Welcome to Qspiders</h1>
     </div>
   </section>
 </template>
